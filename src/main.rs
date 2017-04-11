2af9b769182d64f48038f79b013e9db9b5659f6f
Kartikaya Gupta <kgupta@mozilla.com>
2017-04-11 14:53:38 -0400

    Add generated-file boilerplate. Fixes #4

--- src/main.rs
+++ src/main.rs
@@ -471,6 +471,11 @@ fn main() {
     }
 
     // Showtime!
+    println!(r###"
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+    "###);
     for dep in all_func_deps {
         // Check for `dep` in the ds map. If we don't find it, look up `dep`
         // in the type map, and if we find an alias, look that up in the ds
@@ -481,6 +486,7 @@ fn main() {
             if !converted.ty_params.is_empty() {
                 continue;
             }
+            println!("/* THIS FILE IS GENERATED! DO NOT MODIFY MANUALLY! See https://github.com/jrmuizel/wr-binding! */");
             println!("{}", converted.c_code);
         } else if let Some(alias) = results_ref.type_map.get(&dep) {
             match alias {
@@ -496,16 +502,19 @@ fn main() {
                                                         |code, (value, param)| {
                                                             code.replace(param, value)
                                                         });
+                        println!("/* THIS FILE IS GENERATED! DO NOT MODIFY MANUALLY! See https://github.com/jrmuizel/wr-binding! */");
                         println!("{}", code);
                     }
                 }
                 &TypeAlias::Typedef(ref alias) => {
+                    println!("/* THIS FILE IS GENERATED! DO NOT MODIFY MANUALLY! See https://github.com/jrmuizel/wr-binding! */");
                     println!("typedef {};\n", alias.aliased.c_code);
                 }
             }
         }
     }
     for converted in &results_ref.funcs {
+        println!("/* THIS FILE IS GENERATED! DO NOT MODIFY MANUALLY! See https://github.com/jrmuizel/wr-binding! */");
         println!("{}", converted.c_code);
     }
 }
