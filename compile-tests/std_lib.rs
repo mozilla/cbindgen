be9a8f44654a0dc94e19e60dc8f8d322c63b1ed8
Ryan Hunt <rhunt@eqrion.net>
2017-07-17 22:51:32 -0400

    Rework std_lib test to make it more realistic

--- compile-tests/std_lib.rs
+++ compile-tests/std_lib.rs
@@ -1,5 +1,5 @@
 #[no_mangle]
-extern "C" fn root(a: Vec<String>,
-                   b: Option<i32>,
-                   c: Result<i32, String>)
+extern "C" fn root(a: &Vec<String>,
+                   b: &Option<i32>,
+                   c: &Result<i32, String>)
 { }
