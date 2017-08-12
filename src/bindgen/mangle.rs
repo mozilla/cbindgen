f66e37d809cb672f7c913fa9286fdc815347ac14
Ryan Hunt <rhunt@eqrion.net>
2017-08-12 03:48:13 -0400

    Clean up mangling

--- src/bindgen/mangle.rs
+++ src/bindgen/mangle.rs
@@ -5,66 +5,90 @@
 use bindgen::ir::Type;
 
 pub fn mangle_path(name: &str, generic_values: &[Type]) -> String {
-    assert!(!name.contains("_"));
-
-    let mut out = String::from(name);
-    if !generic_values.is_empty() {
-        out.push_str("_"); // <
-        let mut should_close = false;
-        for (i, ty) in generic_values.iter().enumerate() {
-            if i != 0 {
-                out.push_str("__"); // ,
-            }
-            if should_close {
-                out.push_str("___"); // >
-            }
-            should_close = append_type(ty, &mut out, mangle_path);
-        }
-    }
-    out
+    internal_mangle_path(name, generic_values, true)
 }
 
-fn append_type(ty: &Type, out: &mut String, generic_handler: fn(&str, &[Type]) -> String) -> bool {
-    match ty {
-        &Type::Path(ref path) => {
-            out.push_str(&generic_handler(&path.name, &path.generics));
-            true
+fn internal_mangle_path(name: &str, generic_values: &[Type], last_in_parent: bool) -> String {
+    assert!(!name.contains("_"));
+
+    if generic_values.is_empty() {
+        return String::from(name);
+    }
+
+    let mut out = String::from(name);
+
+    out.push_str("_"); // <
+    for (i, ty) in generic_values.iter().enumerate() {
+        if i != 0 {
+            out.push_str("__"); // ,
         }
-        &Type::Primitive(ref primitive) => {
-            out.push_str(primitive.to_repr_rust());
-            false
+
+        let is_last = i == generic_values.len() - 1;
+        match ty {
+            &Type::Path(ref path) => {
+                out.push_str(&internal_mangle_path(&path.name,
+                                                   &path.generics,
+                                                   last_in_parent && is_last));
+            }
+            &Type::Primitive(ref primitive) => {
+                out.push_str(primitive.to_repr_rust());
+            }
+            &Type::ConstPtr(..) |
+            &Type::Ptr(..) |
+            &Type::Array(..)|
+            &Type::FuncPtr(..) => {
+                unimplemented!()
+            }
         }
-        &Type::ConstPtr(..) => {
-            unimplemented!()
-        }
-        &Type::Ptr(..) => {
-            unimplemented!()
-        }
-        &Type::Array(..) => {
-            unimplemented!()
-        }
-        &Type::FuncPtr(..) => {
-            unimplemented!()
+
+        // Skip writing the trailing '>' mangling when possible
+        if is_last && !last_in_parent {
+            out.push_str("___"); // >
         }
     }
+
+    out
 }
 
 #[test]
 fn mangle_test() {
-    assert_eq!(mangle_path("Foo",
-                           &vec![Type::Primitive(PrimitiveType::Float)]),
+    use bindgen::ir::{GenericPath, PrimitiveType};
+
+    fn float() -> Type {
+        Type::Primitive(PrimitiveType::Float)
+    }
+
+    fn path(path: &str) -> Type {
+        Type::Path(GenericPath::new(path.to_owned(), Vec::new()))
+    }
+
+    fn generic_path(path: &str, generics: &[Type]) -> Type {
+        Type::Path(GenericPath::new(path.to_owned(), generics.to_owned()))
+    }
+
+    /* Foo<f32> => Foo_f32 */
+    assert_eq!(mangle_path("Foo", &vec![float()]),
                "Foo_f32");
 
-    assert_eq!(mangle_path("Foo",
-                           &vec![Type::Path("Bar".to_owned(),
-                                            vec![Type::Primitive(PrimitiveType::Float)])
-                           ]),
+    /* Foo<Bar<f32>> => Foo_Bar_f32 */
+    assert_eq!(mangle_path("Foo", &vec![generic_path("Bar", &[float()])]),
                "Foo_Bar_f32");
 
-    assert_eq!(mangle_path("Vec", &[Type::Path("u8".into(), Vec::new())]), "Vec_u8");
-    assert_eq!(mangle_path("Vec", &[Type::Path("Vec".into(), vec!(Type::Primitive(PrimitiveType::UInt8)))]),
-               "Vec_Vec_u8");
-    assert_eq!(mangle_path("Foo", &[Type::Path("Bar".into(), vec![Type::Path("T".into(), Vec::new()),]),
-                                    Type::Path("E".into(), Vec::new())]),
+    /* Foo<Bar> => Foo_Bar */
+    assert_eq!(mangle_path("Foo", &[path("Bar")]),
+               "Foo_Bar");
+
+    /* Foo<Bar<T>> => Foo_Bar_T */
+    assert_eq!(mangle_path("Foo", &[generic_path("Bar", &[path("T")])]),
+               "Foo_Bar_T");
+
+    // /* Foo<Bar<T>, E> => Foo_Bar_T_____E */
+    assert_eq!(mangle_path("Foo", &[generic_path("Bar", &[path("T")]),
+                                    path("E")]),
                "Foo_Bar_T_____E");
+
+    // /* Foo<Bar<T>, Bar<E>> => Foo_Bar_T_____Bar_E */
+    assert_eq!(mangle_path("Foo", &[generic_path("Bar", &[path("T")]),
+                                    generic_path("Bar", &[path("E")])]),
+               "Foo_Bar_T_____Bar_E");
 }
