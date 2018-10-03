65e39cb7379d2e6fed073409d1e6f1ffdd18ae70
Emilio Cobos Álvarez <emilio@crisal.io>
2018-10-03 20:25:28 +0200

    Fix tagged enum's interactions with prefixing.

--- tests/expectations/prefix.cpp
+++ tests/expectations/prefix.cpp
@@ -7,8 +7,26 @@ using PREFIX_NamedLenArray = int32_t[PREFIX_LEN];
 
 using PREFIX_ValuedLenArray = int32_t[42];
 
+union PREFIX_AbsoluteFontWeight {
+  enum class Tag : uint8_t {
+    Weight,
+    Normal,
+    Bold,
+  };
+
+  struct Weight_Body {
+    Tag tag;
+    float _0;
+  };
+
+  struct {
+    Tag tag;
+  };
+  Weight_Body weight;
+};
+
 extern "C" {
 
-void root(PREFIX_NamedLenArray x, PREFIX_ValuedLenArray y);
+void root(PREFIX_NamedLenArray x, PREFIX_ValuedLenArray y, PREFIX_AbsoluteFontWeight z);
 
 } // extern "C"
