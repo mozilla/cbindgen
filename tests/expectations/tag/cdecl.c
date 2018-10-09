28698482ebecc5b2030c257a5688ab3e29cc6209
Mrmaxmeier <Mrmaxmeier@gmail.com>
2018-10-09 22:21:01 +0200

    test named fn type arguments

--- tests/expectations/tag/cdecl.c
+++ tests/expectations/tag/cdecl.c
@@ -30,6 +30,8 @@ typedef bool (*M[16])(int32_t, int32_t);
 
 typedef void (*N[16])(int32_t, int32_t);
 
+typedef void (*P)(int32_t named1st, bool, bool named3rd, int32_t _);
+
 void (*O(void))(void);
 
-void root(A a, B b, C c, D d, E e, F f, G g, H h, I i, J j, K k, L l, M m, N n);
+void root(A a, B b, C c, D d, E e, F f, G g, H h, I i, J j, K k, L l, M m, N n, P p);
