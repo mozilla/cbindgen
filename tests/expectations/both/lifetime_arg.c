161d83c0b124c8bf5f6d09f767c1aa8cb3308def
Mrmaxmeier <Mrmaxmeier@gmail.com>
2018-10-09 16:53:05 +0200

    add lifetime_arg test

---
+++ tests/expectations/both/lifetime_arg.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct A {
+  const int32_t *data;
+} A;
+
+void root(A _a);
---
+++ tests/expectations/tag/lifetime_arg.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct A {
+  const int32_t *data;
+};
+
+void root(struct A _a);
