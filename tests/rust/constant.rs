b714016e856813e3124e8cda7d8f5921cdb03879
Axel Nennker <axel.nennker@telekom.de>
2019-04-16 03:56:55 +0200

    fix #205
    Signed-off-by: Axel Nennker <axel.nennker@telekom.de>

--- tests/rust/constant.rs
+++ tests/rust/constant.rs
@@ -8,6 +8,9 @@ pub const NEWLINE: char = '\n';
 pub const HEART: char = '❤';
 const ZOM: f32 = 3.14;
 
+pub const POS_ONE: i8 = 1;
+pub const NEG_ONE: i8 = -1;
+
 #[repr(C)]
 struct Foo {
     x: [i32; FOO],
