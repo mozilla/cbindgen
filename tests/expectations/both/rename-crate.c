7431be57d86a94c33bb23d719e3f2a1e8b6ab98d
Alexis Beingessner <a.beingessner@gmail.com>
2019-04-29 15:10:48 -0400

    Add test for package renaming

--- tests/expectations/both/rename-crate.c
+++ tests/expectations/both/rename-crate.c
@@ -3,8 +3,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct RenamedTy {
+  uint64_t y;
+} RenamedTy;
+
 typedef struct Foo {
   int32_t x;
 } Foo;
+
+void renamed_func(RenamedTy a);
 
 void root(Foo a);
--- tests/rust/rename-crate/src/lib.rs
+++ tests/rust/rename-crate/src/lib.rs
@@ -1,7 +1,13 @@
 extern crate dependency as internal_name;
+extern crate renamed_dep;
 
 pub use internal_name::*;
+pub use renamed_dep::*;
 
 #[no_mangle]
 pub extern "C" fn root(a: Foo) {
+}
+
+#[no_mangle]
+pub extern "C" fn renamed_func(a: RenamedTy) {
 }
