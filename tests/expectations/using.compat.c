4bd7b07d2fecfcdc8aa57e41b9fc6497780a7c88
Rasmus Eneman <rasmus@eneman.eu>
2019-08-24 01:09:50 +0200

    Add support for adding "using namespace" statements

---
+++ tests/expectations/using.compat.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+using namespace std;
+
+extern "C" {
+#endif // __cplusplus
+
+void root(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
---
+++ tests/expectations/using.cpp
@@ -0,0 +1,12 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+using namespace std;
+
+extern "C" {
+
+void root();
+
+} // extern "C"
