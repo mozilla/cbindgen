603aa2f9bfcfeeb43c0023f43abf48b0f4adaa5b
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-10-11 22:38:25 +0530

    Add support for --quiet flag

--- src/logging.rs
+++ src/logging.rs
@@ -11,6 +11,7 @@ use log::*;
 pub struct TraceLogger;
 pub struct WarnLogger;
 pub struct InfoLogger;
+pub struct ErrorLogger;
 
 impl TraceLogger {
     pub fn init() -> Result<(), SetLoggerError> {
@@ -45,6 +46,29 @@ impl WarnLogger {
 impl log::Log for WarnLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= Level::Warn
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            writeln!(io::stderr(), "{}: {}", record.level(), record.args()).unwrap();
+        }
+    }
+
+    fn flush(&self) {
+        io::stderr().flush().unwrap();
+    }
+}
+
+impl ErrorLogger {
+    pub fn init() -> Result<(), SetLoggerError> {
+        log::set_logger(&InfoLogger)?;
+        log::set_max_level(LevelFilter::Error);
+        Ok(())
+    }
+}
+impl log::Log for ErrorLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Error
     }
 
     fn log(&self, record: &Record) {
