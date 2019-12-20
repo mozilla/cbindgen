ae5c1b1d6ec3a653b7b05055587921fcfe39fea6
Emilio Cobos Álvarez <emilio@crisal.io>
2019-12-20 23:28:30 +0100

    Commit the expectations of the testcase that used to panic.

---
+++ tests/expectations/expand_dep_v2.cpp
@@ -0,0 +1,15 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+struct dep_struct {
+  uint32_t x;
+  double y;
+};
+
+extern "C" {
+
+uint32_t get_x(const dep_struct *dep_struct);
+
+} // extern "C"
