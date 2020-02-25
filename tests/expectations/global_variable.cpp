61e55ce4d96430ba5bce6e3ed39868a35fba323e
TheKK <thumbd03803@gmail.com>
2020-02-25 13:32:28 +0800

    ir: write declaration of global variable like struct fields

---
+++ tests/expectations/global_variable.cpp
@@ -0,0 +1,12 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+extern "C" {
+
+extern const char CONST_GLOBAL_ARRAY[128];
+
+extern char MUT_GLOBAL_ARRAY[128];
+
+} // extern "C"
