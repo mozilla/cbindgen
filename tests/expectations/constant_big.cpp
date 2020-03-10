7101465403dec23f8752d4b6359e998e89ca3b6b
Joe Devietti <devietti@cis.upenn.edu>
2020-03-09 23:11:51 -0400

    avoid gcc warning by appending ULL suffix to integer literals that are too big to fit in a signed 64-bit integer

---
+++ tests/expectations/constant_big.cpp
@@ -0,0 +1,8 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+static const uint64_t DOESNT_NEED_ULL_SUFFIX = 8070450532247928832;
+
+static const uint64_t NEEDS_ULL_SUFFIX = 9223372036854775808ULL;
---
+++ tests/expectations/tag/constant_big.compat.c
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define DOESNT_NEED_ULL_SUFFIX 8070450532247928832
+
+#define NEEDS_ULL_SUFFIX 9223372036854775808ULL
