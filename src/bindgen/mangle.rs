d0a2226c9f3570098fb356d4fc490c822729989a
Adam Wilson <adam@codegarden.co.uk>
2020-04-01 15:13:02 +0100

    Add option for `mangling_separator` in ExportConfig

--- src/bindgen/mangle.rs
+++ src/bindgen/mangle.rs
@@ -102,7 +102,15 @@ impl<'a> Mangler<'a> {
         if !self.last {
             self.push(Separator::ClosingAngleBracket)
         }
+        }
     }
+
+fn concat_separators(separator: &str, number: u8) -> String {
+    let mut result: String = "".to_string();
+    for _ in 0..number {
+        result += separator;
+    }
+    result
 }
 
 fn concat_separators(separator: &str, number: u8) -> String {
