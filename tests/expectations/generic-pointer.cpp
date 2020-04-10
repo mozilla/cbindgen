33d9ecf11f300bd03f431e9f3417c063e681380c
Emilio Cobos Álvarez <emilio@crisal.io>
2020-04-10 12:50:16 +0200

    Handle mangling pointers.
    Ugly, but better than crashing.
    
    Closes #506.
    Closes #453.

---
+++ tests/expectations/generic-pointer.cpp
@@ -0,0 +1,17 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+template<typename T>
+struct Foo {
+  T a;
+};
+
+using Boo = Foo<uint8_t*>;
+
+extern "C" {
+
+void root(Boo x);
+
+} // extern "C"
