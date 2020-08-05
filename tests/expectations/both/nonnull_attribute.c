afd4a05d4e8d4797beb1111ff5b60e625a2ffe83
Nick Wilcox <ec2-user@ip-172-31-39-31.ap-southeast-2.compute.internal>
2020-08-05 09:04:24 +0000

    update nonnull expectations

--- tests/expectations/both/nonnull_attribute.c
+++ tests/expectations/both/nonnull_attribute.c
@@ -13,40 +13,40 @@
 typedef struct Opaque Opaque;
 
 typedef struct Pointers_u64 {
-  float * CBINDGEN_NONNULL a;
-  uint64_t * CBINDGEN_NONNULL b;
-  Opaque * CBINDGEN_NONNULL c;
-  uint64_t * CBINDGEN_NONNULL * CBINDGEN_NONNULL d;
-  float * CBINDGEN_NONNULL * CBINDGEN_NONNULL e;
-  Opaque * CBINDGEN_NONNULL * CBINDGEN_NONNULL f;
+  float *CBINDGEN_NONNULL a;
+  uint64_t *CBINDGEN_NONNULL b;
+  Opaque *CBINDGEN_NONNULL c;
+  uint64_t *CBINDGEN_NONNULL *CBINDGEN_NONNULL d;
+  float *CBINDGEN_NONNULL *CBINDGEN_NONNULL e;
+  Opaque *CBINDGEN_NONNULL *CBINDGEN_NONNULL f;
   uint64_t *g;
   int32_t *h;
-  int32_t * CBINDGEN_NONNULL *i;
+  int32_t *CBINDGEN_NONNULL *i;
   const uint64_t *j;
   uint64_t *k;
 } Pointers_u64;
 
 typedef struct References {
-  const Opaque * CBINDGEN_NONNULL a;
-  Opaque * CBINDGEN_NONNULL b;
+  const Opaque *CBINDGEN_NONNULL a;
+  Opaque *CBINDGEN_NONNULL b;
   const Opaque *c;
   Opaque *d;
 } References;
 
-void mut_ref_arg(const Pointers_u64 * CBINDGEN_NONNULL arg);
+void mut_ref_arg(Pointers_u64 *CBINDGEN_NONNULL arg);
 
-void mutltiple_args(int32_t * CBINDGEN_NONNULL arg,
+void mutltiple_args(int32_t *CBINDGEN_NONNULL arg,
                     Pointers_u64 *foo,
-                    Opaque * CBINDGEN_NONNULL * CBINDGEN_NONNULL d);
+                    Opaque *CBINDGEN_NONNULL *CBINDGEN_NONNULL d);
 
 void nullable_const_ptr(const Pointers_u64 *arg);
 
 void nullable_mut_ptr(Pointers_u64 *arg);
 
-void optional_mut_ref_arg(const Pointers_u64 *arg);
+void optional_mut_ref_arg(Pointers_u64 *arg);
 
-void optional_ref_arg(Pointers_u64 *arg);
+void optional_ref_arg(const Pointers_u64 *arg);
 
-void ref_arg(Pointers_u64 * CBINDGEN_NONNULL arg);
+void ref_arg(const Pointers_u64 *CBINDGEN_NONNULL arg);
 
 void value_arg(References arg);
--- tests/expectations/tag/nonnull_attribute.c
+++ tests/expectations/tag/nonnull_attribute.c
@@ -13,40 +13,40 @@
 struct Opaque;
 
 struct Pointers_u64 {
-  float * CBINDGEN_NONNULL a;
-  uint64_t * CBINDGEN_NONNULL b;
-  struct Opaque * CBINDGEN_NONNULL c;
-  uint64_t * CBINDGEN_NONNULL * CBINDGEN_NONNULL d;
-  float * CBINDGEN_NONNULL * CBINDGEN_NONNULL e;
-  struct Opaque * CBINDGEN_NONNULL * CBINDGEN_NONNULL f;
+  float *CBINDGEN_NONNULL a;
+  uint64_t *CBINDGEN_NONNULL b;
+  struct Opaque *CBINDGEN_NONNULL c;
+  uint64_t *CBINDGEN_NONNULL *CBINDGEN_NONNULL d;
+  float *CBINDGEN_NONNULL *CBINDGEN_NONNULL e;
+  struct Opaque *CBINDGEN_NONNULL *CBINDGEN_NONNULL f;
   uint64_t *g;
   int32_t *h;
-  int32_t * CBINDGEN_NONNULL *i;
+  int32_t *CBINDGEN_NONNULL *i;
   const uint64_t *j;
   uint64_t *k;
 };
 
 struct References {
-  const struct Opaque * CBINDGEN_NONNULL a;
-  struct Opaque * CBINDGEN_NONNULL b;
+  const struct Opaque *CBINDGEN_NONNULL a;
+  struct Opaque *CBINDGEN_NONNULL b;
   const struct Opaque *c;
   struct Opaque *d;
 };
 
-void mut_ref_arg(const struct Pointers_u64 * CBINDGEN_NONNULL arg);
+void mut_ref_arg(struct Pointers_u64 *CBINDGEN_NONNULL arg);
 
-void mutltiple_args(int32_t * CBINDGEN_NONNULL arg,
+void mutltiple_args(int32_t *CBINDGEN_NONNULL arg,
                     struct Pointers_u64 *foo,
-                    struct Opaque * CBINDGEN_NONNULL * CBINDGEN_NONNULL d);
+                    struct Opaque *CBINDGEN_NONNULL *CBINDGEN_NONNULL d);
 
 void nullable_const_ptr(const struct Pointers_u64 *arg);
 
 void nullable_mut_ptr(struct Pointers_u64 *arg);
 
-void optional_mut_ref_arg(const struct Pointers_u64 *arg);
+void optional_mut_ref_arg(struct Pointers_u64 *arg);
 
-void optional_ref_arg(struct Pointers_u64 *arg);
+void optional_ref_arg(const struct Pointers_u64 *arg);
 
-void ref_arg(struct Pointers_u64 * CBINDGEN_NONNULL arg);
+void ref_arg(const struct Pointers_u64 *CBINDGEN_NONNULL arg);
 
 void value_arg(struct References arg);
