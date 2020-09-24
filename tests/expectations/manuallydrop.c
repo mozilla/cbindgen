79bb38abdaf64fda8edbb3a0afea44bdefd7174e
Ivan Enderlin <ivan@mnt.io>
2020-09-24 14:20:22 +0200

    test: Test `ManuallyDrop` as a simplified type.

---
+++ tests/expectations/manuallydrop.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct NotReprC_ManuallyDrop_Point NotReprC_ManuallyDrop_Point;
+
+typedef NotReprC_ManuallyDrop_Point Foo;
+
+typedef struct {
+  int32_t x;
+  int32_t y;
+} Point;
+
+typedef struct {
+  Point point;
+} MyStruct;
+
+void root(const Foo *a, const MyStruct *with_manual_drop);
+
+void take(Point with_manual_drop);
