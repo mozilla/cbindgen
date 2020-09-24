af6d2e3fbbc2121c4519fd601d7fa6b525298def
Ivan Enderlin <ivan@mnt.io>
2020-09-24 14:28:45 +0200

    test: test `MaybeUninit` as a simplified type.

---
+++ tests/expectations/tag/maybeuninit.compat.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+struct NotReprC_MaybeUninit______i32;
+
+typedef struct NotReprC_MaybeUninit______i32 Foo;
+
+struct MyStruct {
+  const int32_t *number;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+void root(const Foo *a, const struct MyStruct *with_maybe_uninit);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
