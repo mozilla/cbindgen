6f02ca3a9221475d910e96c93d921fdd010224c7
Ivan Enderlin <ivan@mnt.io>
2020-09-28 15:23:05 +0200

    test: Lint Rust code.

--- tests/rust/maybeuninit.rs
+++ tests/rust/maybeuninit.rs
@@ -1,13 +1,13 @@
 #[repr(C)]
-pub struct MyStruct {
-    number: std::mem::MaybeUninit<&i32>,
+pub struct MyStruct<'a> {
+    number: std::mem::MaybeUninit<&'a i32>,
 }
 
 pub struct NotReprC<T> {
     inner: T,
 }
 
-pub type Foo = NotReprC<std::mem::MaybeUninit<&i32>>;
+pub type Foo<'a> = NotReprC<std::mem::MaybeUninit<&'a i32>>;
 
 #[no_mangle]
-pub extern "C" fn root(a: &Foo, with_maybe_uninit: &MyStruct) {}
+pub extern "C" fn root<'a, 'b>(a: &'a Foo, with_maybe_uninit: &'b MyStruct) {}
