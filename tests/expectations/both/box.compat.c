4ce324cd2d7eb5f9bd0578e1fe7530c627227c00
Ivan Enderlin <ivan@mnt.io>
2020-09-29 11:28:13 +0200

    feat: Simplify `Box<T>` to `*T` for C only.

---
+++ tests/expectations/both/box.compat.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct NotReprC_Box_i32 NotReprC_Box_i32;
+
+typedef NotReprC_Box_i32 Foo;
+
+typedef struct MyStruct {
+  int32_t *number;
+} MyStruct;
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+void delete(int32_t *x);
+
+void root(const Foo *a, const MyStruct *with_box);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
