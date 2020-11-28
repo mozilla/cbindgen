8997277cb71922b028a003349c59bd87ae0afe4d
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2020-11-29 02:33:15 +0300

    enum: Break up `Enum::write` into multiple functions

--- tests/expectations/must_use.both.compat.c
+++ tests/expectations/must_use.both.compat.c
@@ -24,7 +24,7 @@ typedef struct Owned_Body_i32 {
   int32_t *_0;
 } Owned_Body_i32;
 
-typedef struct MaybeOwnedPtr_i32 {
+typedef struct MUST_USE_STRUCT MaybeOwnedPtr_i32 {
   MaybeOwnedPtr_i32_Tag tag;
   union {
     Owned_Body_i32 owned;
