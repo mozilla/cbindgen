2731e6d3028d1d5210f6a4d778ef2254abe2160f
Jon Gjengset <jongje@amazon.com>
2021-04-05 15:09:52 -0700

    Pass --filter-platforms to cargo metadata
    cbindgen invokes `cargo metadata` to find the sources of dependencies so
    that it can generate bindings for types that originate in those
    dependencies. However, `cargo metadata` defaults to fetching
    dependencies for _all_ platforms unless it is specifically invoked with
    the `--filter-platforms` argument.
    
    This PR makes cbindgen pass that argument to `cargo metadata` using the
    host platform, which will significantly reduce the time of the first
    call in cases where the dependency tree includes many target-specific
    dependencies.
    
    Fixes #675.

--- src/bindgen/cargo/cargo_metadata.rs
+++ src/bindgen/cargo/cargo_metadata.rs
@@ -185,11 +185,42 @@ pub fn metadata(
     let metadata = match existing_metadata_file {
         Some(path) => Cow::Owned(std::fs::read_to_string(path)?),
         None => {
+            // cargo metadata defaults to giving information for _all_ targets.
+            // We figure out the host platform through rustc and use that.
+            // We unfortunatelly cannot go through cargo, since cargo rustc _also_ builds.
+            // If `rustc` fails to run, we just fall back to not passing --filter-platforms.
+            //
+            // NOTE: We set the current directory in case of rustup shenanigans.
+            let rustc = Command::new("rustc")
+                .current_dir(manifest_path.parent().unwrap())
+                .arg("-vV")
+                .output();
+            log::debug!("Discovering host platform by {:?}", rustc);
+            let mut target = None;
+            if let Ok(out) = rustc {
+                if let Ok(stdout) = std::str::from_utf8(&out.stdout) {
+                    let field = "host: ";
+                    let value = stdout.lines().find_map(|l| l.strip_prefix(field));
+                    if let Some(value) = value {
+                        target = Some(value.to_string());
+                    }
+                }
+            }
+            if target.is_none() {
+                log::warn!(
+                    "Failed to discover host platform for cargo metadata; \
+                    will fetch dependencies for all platforms."
+                );
+            }
+
             let cargo = env::var("CARGO").unwrap_or_else(|_| String::from("cargo"));
             let mut cmd = Command::new(cargo);
             cmd.arg("metadata");
             cmd.arg("--all-features");
             cmd.arg("--format-version").arg("1");
+            if let Some(target) = target {
+                cmd.arg("--filter-platform").arg(target);
+            }
             cmd.arg("--manifest-path");
             cmd.arg(manifest_path);
             output = cmd.output()?;
