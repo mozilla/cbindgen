ccb5d865f8c107c43deac0c69182cf55052f67a6
Jordan Rose <jrose@signal.org>
2022-04-04 11:48:56 -0700

    Fix regression in CamelCase rename rule (should be lowerCamelCase)

---
+++ tests/expectations/rename_case.cpp
@@ -0,0 +1,19 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+extern "C" {
+
+void test_camel_case(int32_t fooBar);
+
+void test_pascal_case(int32_t FooBar);
+
+void test_snake_case(int32_t foo_bar);
+
+void test_screaming_snake_case(int32_t FOO_BAR);
+
+void test_gecko_case(int32_t aFooBar);
+
+} // extern "C"
