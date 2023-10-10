52b95f1b4b1b548308cac7da4bbb8fcfaa65937a
orangeng <ngquanhao@outlook.com>
2023-10-10 01:35:28 -0500

    Resolve fully-qualified associated types

---
+++ tests/expectations/assoc_types_nested.c
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+bool test_nested_assoc(bool out, bool mid);
+
+void test_double_assoc(bool double_[10]);
---
+++ tests/expectations/assoc_types_nested.compat.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+bool test_nested_assoc(bool out, bool mid);
+
+void test_double_assoc(bool double_[10]);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
