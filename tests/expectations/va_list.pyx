785e066e03b0e22a32c31e9adab78a18f6ce195b
Kan-Ru Chen <kanru@kanru.info>
2024-05-29 08:58:13 +0900

    Fix variadic arguments when used in function pointer

--- tests/expectations/va_list.pyx
+++ tests/expectations/va_list.pyx
@@ -8,21 +8,18 @@ cdef extern from *:
 
   ctypedef int32_t (*VaListFnPtr)(int32_t count, ...);
 
-  ctypedef int32_t (*VaListFnPtr2)(int32_t count);
+  ctypedef int32_t (*VaListFnPtr2)(int32_t count, ...);
 
   ctypedef struct Interface_______i32_______i32_______va_list:
     int32_t (*fn1)(int32_t count, ...);
-
-  ctypedef struct Interface_______i32_______i32:
-    int32_t (*fn1)(int32_t count);
 
   int32_t va_list_test(int32_t count, ...);
 
   int32_t va_list_test2(int32_t count, ...);
 
   void va_list_fn_ptrs(int32_t (*fn1)(int32_t count, ...),
-                       int32_t (*fn2)(int32_t count),
+                       int32_t (*fn2)(int32_t count, ...),
                        VaListFnPtr fn3,
                        VaListFnPtr2 fn4,
                        Interface_______i32_______i32_______va_list fn5,
-                       Interface_______i32_______i32 fn6);
+                       Interface_______i32_______i32_______va_list fn6);
